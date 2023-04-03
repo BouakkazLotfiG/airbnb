b4f5a668a590f126a358d1127ac27677c9fbd0c2
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-03 01:32:49 +0100

    started navbar with logo done responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
 
+import Navbar from './components/navbar/Navbar';
+
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb clone',
@@ -17,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
