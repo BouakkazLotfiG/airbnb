36f6320a7e4f1e8d3277b3b682e5257819beaa7e
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-17 15:31:56 +0100

    modal and button ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import { Nunito } from 'next/font/google';
 
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
 
 export const metadata = {
   title: 'Airbnb',
@@ -20,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal actionLabel='Submit' isOpen={true} />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
