87ba0b1a2fed179a35ebcc7379b616219dc9a83c
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-24 11:33:57 +0100

    categories done

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
+import Categories from './Categories';
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -35,6 +36,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
