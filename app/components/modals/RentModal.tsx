c03b76a202042255f32578c0740e003b35277c47
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-24 13:27:42 +0100

    Information page for rental , counter

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -3,12 +3,14 @@
 import { useMemo, useState } from 'react';
 import { categories } from '../navbar/Categories';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import dynamic from 'next/dynamic';
 
 import Modal from './Modal';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import Input from '../inputs/Input';
 import Heading from '../Heading';
+import Counter from '../inputs/Counter';
 
 import useRentModal from '../hooks/useRentModal';
 import Map from '../Map';
@@ -53,6 +55,14 @@ const RentModal = () => {
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
   const imageSrc = watch('imageSrc');
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -139,24 +149,44 @@ const RentModal = () => {
           title='Share some basics about your place'
           subtitle='What amenitis do you have?'
         />
+        <Counter
+          onChange={(value) => setCustomValue('guestCount', value)}
+          value={guestCount}
+          title='Guests'
+          subtitle='How many guests do you allow?'
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title='Rooms'
+          subtitle='How many rooms do you have?'
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title='Bathrooms'
+          subtitle='How many bathrooms do you have?'
+        />
       </div>
     );
   }
 
-  //   if (step === STEPS.IMAGES) {
-  //     bodyContent = (
-  //       <div className='flex flex-col gap-8'>
-  //         <Heading
-  //           title='Add a photo of your place'
-  //           subtitle='Show guests what your place looks like!'
-  //         />
-  //         <ImageUpload
+  // if (step === STEPS.IMAGES) {
+  //   bodyContent = (
+  //     <div className='flex flex-col gap-8'>
+  //       <Heading
+  //         title='Add a photo of your place'
+  //         subtitle='Show guests what your place looks like!'
+  //       />
+  //       {/* <ImageUpload
   //           onChange={(value) => setCustomValue('imageSrc', value)}
   //           value={imageSrc}
-  //         />
-  //       </div>
-  //     );
-  //   }
+  //         /> */}
+  //     </div>
+  //   );
+  // }
 
   //   if (step === STEPS.DESCRIPTION) {
   //     bodyContent = (
