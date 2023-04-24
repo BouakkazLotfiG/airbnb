4e976099bc85d2e72e9c1b7e9beea971d926488e
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-24 13:57:52 +0100

    imge uplad of the airbnb with cloudinary

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from '../inputs/CountrySelect';
 import Input from '../inputs/Input';
 import Heading from '../Heading';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 import useRentModal from '../hooks/useRentModal';
 import Map from '../Map';
@@ -173,20 +174,20 @@ const RentModal = () => {
     );
   }
 
-  // if (step === STEPS.IMAGES) {
-  //   bodyContent = (
-  //     <div className='flex flex-col gap-8'>
-  //       <Heading
-  //         title='Add a photo of your place'
-  //         subtitle='Show guests what your place looks like!'
-  //       />
-  //       {/* <ImageUpload
-  //           onChange={(value) => setCustomValue('imageSrc', value)}
-  //           value={imageSrc}
-  //         /> */}
-  //     </div>
-  //   );
-  // }
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like!'
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
+        />
+      </div>
+    );
+  }
 
   //   if (step === STEPS.DESCRIPTION) {
   //     bodyContent = (
