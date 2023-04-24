2989c9abc10382cf9e8151ed64d3859e86a260e9
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-24 16:54:16 +0100

    listing card done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
-export default function Home() {
-  return <div className='text-2xl text-red-400'>sup</div>;
-}
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import ClientOnly from './components/ClientOnly';
+import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+
+import Container from './components/Container';
+
+const Home = async () => {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className='
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          '
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
+};
+
+export default Home;
