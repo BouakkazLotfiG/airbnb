f2dffd75726d3fdbc783a228e68aa48c4557d033
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-04-24 13:16:36 +0100

    country select with map

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import Input from '../inputs/Input';
 import Heading from '../Heading';
 
 import useRentModal from '../hooks/useRentModal';
+import Map from '../Map';
 
 enum STEPS {
   CATEGORY = 0,
@@ -126,40 +127,21 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
+        <Map center={location?.latlng} />
       </div>
     );
   }
 
-  //   if (step === STEPS.INFO) {
-  //     bodyContent = (
-  //       <div className='flex flex-col gap-8'>
-  //         <Heading
-  //           title='Share some basics about your place'
-  //           subtitle='What amenitis do you have?'
-  //         />
-  //         <Counter
-  //           onChange={(value) => setCustomValue('guestCount', value)}
-  //           value={guestCount}
-  //           title='Guests'
-  //           subtitle='How many guests do you allow?'
-  //         />
-  //         <hr />
-  //         <Counter
-  //           onChange={(value) => setCustomValue('roomCount', value)}
-  //           value={roomCount}
-  //           title='Rooms'
-  //           subtitle='How many rooms do you have?'
-  //         />
-  //         <hr />
-  //         <Counter
-  //           onChange={(value) => setCustomValue('bathroomCount', value)}
-  //           value={bathroomCount}
-  //           title='Bathrooms'
-  //           subtitle='How many bathrooms do you have?'
-  //         />
-  //       </div>
-  //     );
-  //   }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Share some basics about your place'
+          subtitle='What amenitis do you have?'
+        />
+      </div>
+    );
+  }
 
   //   if (step === STEPS.IMAGES) {
   //     bodyContent = (
